24bb436e32158057b2ceec592efa8d86257c6c30
RafaAlbano <heloisalopesalbano@gmail.com>
2022-10-13 10:57:46 -0300

    imagens sofro

--- src/views/FilmesView.vue
+++ src/views/FilmesView.vue
@@ -13,13 +13,29 @@ export default {
   },
   methods: {
     ...mapActions(useResultStore, ["get_results"]),
+    getImgUrl(poster_path) {
+      return `https://image.tmdb.org/t/p/w500${poster_path}`;
+    },
+    mostraFilme(filme) {
+      alert(filme.id);
+    },
   },
 };
 </script>
 <template>
   <CarroselComp />
-  <p v-for="result of results" :key="result.id">
-    {{ result.title }} - {{ result.id }}
-  </p>
+  <div class="filmes" v-for="result of results" :key="result.id">
+    <p>{{ result.title }} - {{ result.id }}</p>
+    <img :src="getImgUrl(result.poster_path)" alt="" />
+  </div>
 </template>
-<style></style>
+<style>
+.filmes {
+  display: inline-block;
+  /* max-width: 350px;
+  max-height: 500px;
+  width: auto;
+  height: auto;
+  margin: 2% 3%; */
+}
+</style>
